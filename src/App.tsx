9f7909825eb655c838a2868e74a9b6bc64b2b41b
Ayush Wostey <wostiayush62@gmail.com>
2026-04-23 12:31:54 +0545

    chore(frontend): initialize React Vite TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1>Vehicle Parts Management System</h1>
+      <p>Frontend is currently under development.</p>
+    </div>
+  );
+}
+
+export default App;
